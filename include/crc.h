8d86874f25fa54199c581bb66a20c688a2095132
yinpo <davyinpo@gmail.com>
2025-04-07 15:27:15 +0800

    fix: remove buffer

--- include/crc.h
+++ include/crc.h
@@ -6,47 +6,47 @@
 #include <unordered_map>
 #include <vector>
 
-class CRC {
+class CRC
+{
 
 public:
-    struct CRCParams {
-        int bitWidth;
-        uint64_t polynomial;
-        uint64_t initialValue;
-        uint64_t finalXorValue;
-        bool refIn;
-        bool refOut;
-    };
+  struct CRCParams
+  {
+    int bitWidth;
+    uint64_t polynomial;
+    uint64_t initialValue;
+    uint64_t finalXorValue;
+    bool refIn;
+    bool refOut;
+  };
 
-    explicit CRC(const std::string &predefined);
+  explicit CRC(const std::string &predefined);
 
-    explicit CRC(const CRCParams &crcParams);
+  explicit CRC(const CRCParams &crcParams);
 
-    static std::vector<std::string> getPoly();
+  static std::vector<std::string> getPoly();
 
-    [[nodiscard]] TransformOut string(const std::string &data);
+  [[nodiscard]] TransformOut string(const std::string &data);
 
-    [[nodiscard]] TransformOut file(const std::string &filePath);
-
-    [[nodiscard]] TransformOut buffer(const char *buffer, size_t length);
+  [[nodiscard]] TransformOut file(const std::string &filePath);
 
 private:
-    std::vector<uint64_t> table;
-    std::array<uint8_t, 256> reverseTable{};
-    CRCParams params{};
-    uint64_t mask;
-    bool bigTable = false;
-    static const std::unordered_map<std::string, CRCParams> predefinedParams;
+  std::vector<uint64_t> table;
+  std::array<uint8_t, 256> reverseTable{};
+  CRCParams params{};
+  uint64_t mask;
+  bool bigTable = false;
+  static const std::unordered_map<std::string, CRCParams> predefinedParams;
 
-    uint64_t reverseBits(uint64_t value, int bitWidth = -1) const;
+  uint64_t reverseBits(uint64_t value, int bitWidth = -1) const;
 
-    uint64_t singleCRC(uint64_t data) const;
+  uint64_t singleCRC(uint64_t data) const;
 
-    void generateCrcTable();
+  void generateCrcTable();
 
-    // [[nodiscard]] uint64_t calculateCRC(const std::vector<uint8_t> &data)
-    // const;
-    [[nodiscard]] uint64_t calculateCRC(uint8_t data, uint64_t crc) const;
+  // [[nodiscard]] uint64_t calculateCRC(const std::vector<uint8_t> &data)
+  // const;
+  [[nodiscard]] uint64_t calculateCRC(uint8_t data, uint64_t crc) const;
 
-    [[nodiscard]] uint64_t blockCRC(uint64_t data, uint64_t crc) const;
+  [[nodiscard]] uint64_t blockCRC(uint64_t data, uint64_t crc) const;
 };
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,14 @@
 #include "./tests.h"
 #include <process.h>
 
-int main() {
-    // test_crc();
-//   Tests::test_input();
-//  Tests::test_path();
-    Tests::test_buffer();
-    // Tests::test_custom();
-    // Tests::test_crc();
-    // 暂停终端
-    // system("pause");
-    return 0;
+int main()
+{
+  // test_crc();
+  //   Tests::test_input();
+  Tests::test_path();
+  // Tests::test_custom();
+  // Tests::test_crc();
+  // 暂停终端
+  // system("pause");
+  return 0;
 }
